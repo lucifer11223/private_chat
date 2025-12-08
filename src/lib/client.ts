63d17d0982e74c902745421bd1a56070bf1957ce
lucifer11223 <1oshshank@gmail.com>
2025-12-08 22:16:08 +0530

    edited client.ts

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,5 +1,9 @@
 import { treaty } from '@elysiajs/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
-// this require .api to enter /api prefix
-export const client = treaty<App>('localhost:3000').api
+const baseURL =
+    typeof window === "undefined"
+        ? process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+        : window.location.origin;
+
+export const client = treaty<App>(baseURL).api;
